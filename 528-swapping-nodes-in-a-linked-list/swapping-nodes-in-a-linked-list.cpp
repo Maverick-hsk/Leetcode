e79c7f77863d3e35af2478479b4c2535ea5ad057
Himank Singh <110485567+Maverick-hsk@users.noreply.github.com>
2026-01-24 10:11:26 +0530

    Time: 3 ms (43.62%) | Memory: 185.4 MB (12.38%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -11,24 +11,39 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        int count =0;
-        ListNode* temp = head;
-        while(temp!= NULL){
-            count++;
-            temp = temp->next;
-        }
-       ListNode* first = head;
-       ListNode* second = head;
 
-      int desired = count - k+1;
-      for(int i = 1;i<k;i++){
-        first = first->next;
-      }
-    for(int i = 1;i<desired;i++){
-        second = second->next;
-    }
-       swap(first->val, second->val);
+    //     int count =0;
+    //     ListNode* temp = head;
+    //     while(temp!= NULL){
+    //         count++;
+    //         temp = temp->next;
+    //     }
+    //    ListNode* first = head;
+    //    ListNode* second = head;
+
+    //   int desired = count - k+1;
+    //   for(int i = 1;i<k;i++){
+    //     first = first->next;
+    //   }
+    // for(int i = 1;i<desired;i++){
+    //     second = second->next;
+    // }
+    //    swap(first->val, second->val);
        
-       return head;
-           }
+    //    return head;
+
+   ListNode* fast = head;
+   ListNode* slow = head;
+   while(--k){
+    fast = fast->next;
+   }
+   ListNode* first = fast;
+   while( fast->next != NULL){
+    fast = fast->next;
+    slow = slow->next;
+   }
+    swap(first->val, slow->val);
+
+    return head;
+    }
 };
