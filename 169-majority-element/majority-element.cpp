3842c3d6397359a4eb455de45976c97faf998cc6
Himank Singh <110485567+Maverick-hsk@users.noreply.github.com>
2026-01-26 08:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (86.45%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,7 +3,25 @@ public:
     int majorityElement(vector<int>& nums) {
 
       int n=  nums.size();
-      int ele = INT_MIN;
+      int ele = nums[0];
+     int count = 0;
+      for(int i=0;i<n;i++){
+        if(ele == nums[i]){
+            count++;
+        }
+        else if(count == 0){
+            count = 1;
+            ele = nums[i];
+        }
+        else{
+            count--;
+        }
+      }
+      for(int i =0;i<n;i++){
+        if(ele == nums[i]) count++;
+      }
+      
+      if(count> n/2) ele = ele;
         // for(int i=0;i<n;i++){
         //     int count =  0;
         //     for(int j=0;j<n;j++){
@@ -16,16 +34,18 @@ public:
         // return ele;
 
 
-    unordered_map<int,int> umap;
-    for(int i = 0;i<n;i++){
-        umap[nums[i]]++;
-    }
+    // unordered_map<int,int> umap;
+    // for(int i = 0;i<n;i++){
+    //     umap[nums[i]]++;
+    // }
     
-    for(auto it: umap){
-        if(it.second > n/2){
-        ele = it.first;
-        }
-    }
+    // for(auto it: umap){
+    //     if(it.second > n/2){
+    //     ele = it.first;
+    //     }
+    // }
+
+    // return ele;
 
     return ele;
     }
