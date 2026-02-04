78a1f5273e1fa2f1be4f71226ca6c17ea506d1ab
Himank Singh <110485567+Maverick-hsk@users.noreply.github.com>
2026-02-04 14:14:55 +0530

    Time: 55 ms (66.37%) | Memory: 24 MB (24.21%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,23 +8,51 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> s;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
         ListNode* curr = headA;
-        while(curr!= NULL){
-            s.insert(curr);
-            curr= curr->next;
+        ListNode* temp = headB;
+        int count_1 = 0;
+        int count_2 = 0;
+        while (curr != NULL) {
+            count_1++;
+            curr = curr->next;
         }
-         
-         ListNode* temp = headB;
-        while(temp!= NULL){
-          if(s.find(temp)!= s.end()){
+        while (temp != NULL) {
+            count_2++;
+            temp = temp->next;
+        }
+        int diff = abs(count_1 - count_2);
+        while (diff--) {
+            if (count_2 > count_1) {
+                p2 = p2->next;
+            } else
+                p1 = p1->next;
+        }
+        while(p1 != NULL && p2!= NULL){
+            if(p1 == p2) return p1;
+            p1 = p1->next;
+            p2= p2->next;
+        }
 
-            return temp;
-            break;
-          }
-          temp  = temp->next;
-        }
         return NULL;
+        // unordered_set<ListNode*> s;
+        // ListNode* curr = headA;
+        // while(curr!= NULL){
+        //     s.insert(curr);
+        //     curr= curr->next;
+        // }
+
+        //  ListNode* temp = headB;
+        // while(temp!= NULL){
+        //   if(s.find(temp)!= s.end()){
+
+        //     return temp;
+        //     break;
+        //   }
+        //   temp  = temp->next;
+        // }
+        // return NULL;
     }
 };
